5704ae87553505685c053bb825b156d22c3fe7f8
Garvit <garvittomar16092008@gmail.com>
2026-08-14 22:45:12 +0530

    Create ArraysListExample.java

---
+++ Arrays/src/com/Garvit/ArraysListExample.java
@@ -0,0 +1,12 @@
+package com.Garvit;
+
+import java.util.ArrayList;
+
+public class ArraysListExample {
+    public static void main(String[] args) {
+        //Syntax:
+        ArrayList<Integer> list = new ArrayList<>(10);
+                
+
+    }
+}
