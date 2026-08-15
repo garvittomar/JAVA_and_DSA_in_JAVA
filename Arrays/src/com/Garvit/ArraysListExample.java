cec41258fbfdacc8791b4db7cc53543a345ec229
Garvit <garvittomar16092008@gmail.com>
2026-08-15 18:00:42 +0530

    Arrays

--- Arrays/src/com/Garvit/ArraysListExample.java
+++ Arrays/src/com/Garvit/ArraysListExample.java
@@ -1,12 +1,44 @@
 package com.Garvit;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArraysListExample {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
         //Syntax:
-        ArrayList<Integer> list = new ArrayList<>(10);
-                
+       ArrayList<Integer> list = new ArrayList<>(10);
+
+
+       //       list.add(67);
+//        list.add(67432);
+//        list.add(6732);
+//        list.add(6743);
+//        list.add(671);
+//        list.add(6723);
+//
+//        //  System.out.println(list.contains(626571));
+//        System.out.println(list);
+//        list.set(0,99);
+//
+//        list.remove(2);
+//
+//        System.out.println(list);
+
+
+          //input:
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+              //get item at any index
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i)); // pass index here ,
+                                             // list[index] syntax will not work here
+        }
+
+       //  System.out.println(list);
 
     }
 }
---
+++ Arrays/src/com/Garvit/Maximum_item.java
@@ -0,0 +1,55 @@
+package com.Garvit;
+
+public class Maximum_item {
+    public static void main(String[] args) {
+
+        // Q. MAximum value of an array:
+
+        int[] arr = {1,3,2,9,18};
+        System.out.println(max(arr)); //overall max value
+        System.out.println(maxRange(arr,3,4));
+
+    }
+
+// Find the max value in range like in between index 1 to 3 or any other
+    //also work on edge cases here , like array being null
+
+    static int maxRange(int[] arr , int start , int end) {
+
+        if(end>start){
+            return -1;
+        }
+
+        if(arr==null){
+            return -1;
+        }
+
+
+        int maxVal =arr[start];
+        for (int i = 0; i < end; i++) {
+            if(arr[i]>maxVal){
+                maxVal=arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+
+
+    static int max(int[] arr) {
+
+        if(arr.length == 0){
+        return -1;
+         }
+
+        int maxVal =arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>maxVal){
+                maxVal=arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+}
+
