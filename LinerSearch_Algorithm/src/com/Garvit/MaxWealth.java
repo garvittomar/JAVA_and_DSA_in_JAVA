bc62a1614a703862f83e607f9394ff71d6e87d35
Garvit <188227678+garvittomar@users.noreply.github.com>
2026-08-18 22:06:29 +0530

    Linear Search algo. and some question

---
+++ LinerSearch_Algorithm/src/com/Garvit/MaxWealth.java
@@ -0,0 +1,26 @@
+package com.Garvit;
+// Q.  https://leetcode.com/problems/richest-customer-wealth/
+public class MaxWealth {
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts) {
+        //person=row
+        //account=col
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            //when you start a new col , take a new sum for that row
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+
+            }
+            // now we have sum of accounts of person
+            //check with overall answer
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+     return ans;
+    }
+}
